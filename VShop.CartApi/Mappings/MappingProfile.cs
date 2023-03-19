5d0c5281bdfac82e07f6cf9af5fef760e95149d2
Norton Almeida Pontes <nortonalmeida08@gmail.com>
2023-03-19 20:54:26 -0300

    Feitos Aula 13 //Macoratti

---
+++ VShop.CartApi/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using VShop.CartApi.DTOs;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<CartDTO,Cart>().ReverseMap();
+        CreateMap<CartHeaderDTO,CartHeader>().ReverseMap();
+        CreateMap<CartItemDTO,CartItem>().ReverseMap();
+        CreateMap<ProductDTO,Product>().ReverseMap();
+    }
+}
---
+++ VShop.CartApi/Repositories/CartRepository.cs
@@ -0,0 +1,172 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using VShop.CartApi.Context;
+using VShop.CartApi.DTOs;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Repositories;
+
+public class CartRepository : ICartRepository
+{
+    private readonly AppDbContext _context;
+    private IMapper _mapper;
+
+    public CartRepository(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<CartDTO> GetCartByUserIdAsync(string userId)
+    {
+        Cart cart = new Cart
+        {
+            //obter o header pelo userId
+            CartHeader = await _context.CartHeaders
+                    .FirstOrDefaultAsync(c => c.UserId == userId)
+        };
+
+        //obter os itens
+        cart.CartItems =  _context.CartItems
+                    .Where(c => c.CartHeaderId == cart.CartHeader.Id)
+                    .Include(p => p.Product);
+
+        return _mapper.Map<CartDTO>(cart);
+    }
+
+    public async Task<bool> DeleteItemCartAsync(int cartItemId)
+    {
+        try
+        {
+            CartItem cartItem = await _context.CartItems
+                            .FirstOrDefaultAsync(c => c.Id == cartItemId);
+
+            int total = _context.CartItems.Where(c => c.CartHeaderId == cartItem.CartHeaderId).Count();
+
+            _context.CartItems.Remove(cartItem);
+
+            if (total == 1)
+            {
+                var cartHeaderRemove = await _context.CartHeaders
+                    .FirstOrDefaultAsync(c => c.Id == cartItem.CartHeaderId);
+
+                _context.CartHeaders.Remove(cartHeaderRemove);
+            }
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> CleanCartAsync(string userId)
+    {
+        var cartHeader = await _context.CartHeaders
+                    .FirstOrDefaultAsync(c => c.UserId == userId);
+
+        if(cartHeader != null)
+        {
+            _context.CartItems.RemoveRange(_context.CartItems
+                .Where(c => c.CartHeaderId == cartHeader.Id));
+
+            _context.CartHeaders.Remove(cartHeader);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<CartDTO> UpdateCartAsync(CartDTO cartDto)
+    {
+        Cart cart = _mapper.Map<Cart>(cartDto);
+
+        //salva o produto no banco se ele não existir
+        await SaveProductInDataBase(cartDto, cart);
+
+        //Verifica se o CartHeader é null
+        var cartHeader = await _context.CartHeaders.AsNoTracking().FirstOrDefaultAsync(
+                                c => c.UserId == cart.CartHeader.UserId);
+
+        if(cartHeader != null)
+        {
+            //criar o header e os itens
+            await CreateHeaderAndItems(cart);
+        }
+        else
+        {
+            await UpdateQuantityAndItems(cartDto, cart, cartHeader);
+        }
+
+        return _mapper.Map<CartDTO>(cart);
+    }
+    public async Task SaveProductInDataBase(CartDTO cartDto, Cart cart)
+    {
+        //Verificase o produto já foi salvo senão salva
+        var product = await _context.Products
+            .FirstOrDefaultAsync(p => p.Id == 
+                cartDto.CartItems.FirstOrDefault().ProductId);
+
+        if (product != null)
+        {
+            _context.Products.Add(cart.CartItems.FirstOrDefault().Product);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task CreateHeaderAndItems( Cart cart)
+    {
+        //Cria o CartHeader e o CartItems
+        _context.CartHeaders.Add(cart.CartHeader);
+        await _context.SaveChangesAsync();
+
+        cart.CartItems.FirstOrDefault().CartHeaderId = cart.CartHeader.Id;
+        cart.CartItems.FirstOrDefault().Product = null;
+
+        _context.CartItems.Add(cart.CartItems.FirstOrDefault());
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateQuantityAndItems(CartDTO cartDto, Cart cart, CartHeader cartHeader)
+    {
+        //Se o CartHeader não é null
+        //verifica se o CartItems possui o mesmo produto
+
+        var cartDetail = await _context.CartItems.AsNoTracking().FirstOrDefaultAsync(
+                            p => p.ProductId == cartDto.CartItems.FirstOrDefault()
+                            .ProductId && p.CartHeaderId == cartHeader.Id);
+
+        if(cartDetail != null)
+        {
+            //Cria o CartItems
+            cart.CartItems.FirstOrDefault().CartHeaderId = cartHeader.Id;
+            cart.CartItems.FirstOrDefault().Product = null;
+            _context.CartItems.Add(cart.CartItems.FirstOrDefault());
+            await _context.SaveChangesAsync();
+        }
+        else
+        {
+            //Atualiza a quantidade e o CartItems
+            cart.CartItems.FirstOrDefault().Product = null;
+            cart.CartItems.FirstOrDefault().Quantity += cartDetail.Quantity;
+            cart.CartItems.FirstOrDefault().Id = cartDetail.Id;
+            cart.CartItems.FirstOrDefault().CartHeaderId = cartDetail.CartHeaderId;
+            _context.CartItems.Update(cart.CartItems.FirstOrDefault());
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<bool> ApplyCouponAsync(string userId, string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCouponAsync(string userId)
+    {
+        throw new NotImplementedException();
+    }
+}
