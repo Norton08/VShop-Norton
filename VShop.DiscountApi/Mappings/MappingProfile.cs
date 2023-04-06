9962ec3148b57fe05a2e00e63651d8b4aa667005
Norton Almeida Pontes <nortonalmeida08@gmail.com>
2023-04-06 08:28:28 -0300

    Feitos Aula 16 //Macoratti

---
+++ VShop.DiscountApi/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VShop.DiscountApi.DTOs;
+using VShop.DiscountApi.Models;
+
+namespace VShop.DiscountApi.Mappings;
+
+public class MappingProfile: Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<CouponDTO, Coupon>().ReverseMap();
+    }
+}
